94ecfd212afc57208a8d52ae5b62019d10221f1c
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-14 20:46:24 -0300

    Create ex2.js

---
+++ EstruturaDeDecisao/ex2.js
@@ -0,0 +1,9 @@
+function IsPositive (number) {
+  if (number > 0) {
+    console.log("Positivo");
+  } else if (number < 0) {
+    console.log("Negativo);
+                } else (number = 0) {
+      console.log("Zero");
+    }
+  }
