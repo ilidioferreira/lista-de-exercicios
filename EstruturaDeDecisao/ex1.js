8a8691fed061051f2f122add7e5e66fd14ab5985
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-14 20:52:55 -0300

    Update ex1.js

--- EstruturaDeDecisao/ex1.js
+++ EstruturaDeDecisao/ex1.js
@@ -1,7 +1,7 @@
 function LargeNumber (num1,num2) {
   if (num1 > num2) {
   console.log(num1);
-  }
-  else if (num2 > num1) { 
-    console.logprint (num2);
+  } else if (num2 > num1) { 
+    console.log(num2);
+} else console.log ("Os números são iguais")
 }
