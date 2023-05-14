a07cb7e18ac36b5fdfc0f817e92c92e5735790ca
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-14 20:51:18 -0300

    Create ex3.js

---
+++ EstruturaDeDecisao/ex3.js
@@ -0,0 +1,7 @@
+function PrintGender (letter) {
+  if (letter == "F") {
+    console.log("Feminino");
+  } else if (letter == "M") {
+    console.log("Masculino");
+  } else console.log("Sexo Inválido");
+}
