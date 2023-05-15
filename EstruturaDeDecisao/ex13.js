be53a11d89f448b6b5ad92cbd871364bcae8f613
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-14 22:17:46 -0300

    Create ex13.js

---
+++ EstruturaDeDecisao/ex13.js
@@ -0,0 +1,17 @@
+function DayOfWeek (number) {
+  if(number == 1) {
+    console.log("Domingo");
+  } else if (number == 2) {
+    console.log("Segunda");
+  } else if (number == 3) {
+    console.log("Terça");
+  } else if (number == 4) {
+    console.log("Quarta");
+  } else if (number == 5) {
+    console.log("Quinta");
+  } else if (number == 6) {
+    console.log("Sexta");
+  } else if (number == 7) {
+    console.log("Sábado");
+  } else console.log("Valor inválido!");
+}
