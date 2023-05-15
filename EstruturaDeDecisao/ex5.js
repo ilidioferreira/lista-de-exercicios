a36e7ebd3a63c46df5f31cb20e4f10661f25c99c
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-14 21:19:46 -0300

    Create ex5.js

---
+++ EstruturaDeDecisao/ex5.js
@@ -0,0 +1,8 @@
+function NotaFinal (n1,n2) {
+  if ( n1 + n2 = 10 ) {
+    console.log("Aprovado com Distinção");
+  } else if ( n1 + n2 >= 7 ) {
+    console.log("Aprovado");
+  } else console.log("Reprovado");
+}
+    
