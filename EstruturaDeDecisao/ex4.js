a0b767452f7be4a4b0bd732d128edd5e26d40fb0
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-14 21:12:01 -0300

    Create ex4.js

---
+++ EstruturaDeDecisao/ex4.js
@@ -0,0 +1,6 @@
+function LetterType (letter) {
+  letter = letter.toLowerCase()
+  if (letter == 'a' || letter == 'b' || letter == 'c' || letter == 'd' || letter == 'e') {
+    console.log("Vogal");
+  } else console.log("Consoante");
+}
