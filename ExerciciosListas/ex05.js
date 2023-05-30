48b358ef5f9565f250fccd8b29db72a5d93846ba
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-05-30 13:27:26 -0300

    feat: exercicios de listas

--- ExerciciosListas/ex05.js
+++ ExerciciosListas/ex05.js
@@ -1,7 +1,8 @@
 // Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
 
+const mensage = prompt('Digite um número inteiro');
+
 function getNumbersLists(number) {
-    const mensage = prompt('Digite um número inteiro');
     const numbers = []
     const even = []
     const odd = []
--- ExerciciosListas/ex14.js
+++ ExerciciosListas/ex14.js
@@ -9,39 +9,49 @@
 // Caso contrário, ele será classificado como "Inocente".
 
 
-const trueQuestions = 0 
+let trueQuestions = 0 
 const question_1 = prompt("Telefonou para a vítima? Digite 'sim' ou 'não'");
+const question_2 = prompt("Esteve no local do crime? Digite 'sim' ou 'não'");
+const question_3 = prompt("Mora perto da vítima? Digite 'sim' ou 'não'");
+const question_4 = prompt("Devia para a vítima? Digite 'sim' ou 'não'");
+const question_5 = prompt("Já trabalhou com a vítima? Digite 'sim' ou 'não'");
 
 while (question_1 != "sim" && question_1 != "não") {
-    question_1 = 
-    prompt("Telefonou para a vítima? Digite 'sim' ou 'não'");
+    question_1;
     if (question_1 === "sim") {
         trueQuestions =+ 1 
     }
 }
 
-
-if (boolean(prompt("Telefonou para a vítima?"))) {
-        trueQuestions =+ 1
+while (question_2 != "sim" && question_2 != "não") {
+    question_2;
+    if (question_2 === "sim") {
+        trueQuestions =+ 1 
     }
-
-if (boolean(prompt("Esteve no local do crime?"))) {
-    trueQuestions =+ 1
 }
 
-if (boolean(prompt("Mora perto da vítima?"))) {
-    trueQuestions =+ 1
+while (question_3 != "sim" && question_3 != "não") {
+    question_3;
+    if (question_3 === "sim") {
+        trueQuestions =+ 1 
+    }
 }
 
-if (boolean(prompt("Devia para a vítima?"))) {
-    trueQuestions =+ 1
+while (question_4 != "sim" && question_4 != "não") {
+    question_4;
+    if (question_4 === "sim") {
+        trueQuestions =+ 1 
+    }
 }
 
-if (boolean(prompt("Já trabalhou com a vítima?"))) {
-    trueQuestions =+ 1
+while (question_5 != "sim" && question_5 != "não") {
+    question_5;
+    if (question_5 === "sim") {
+        trueQuestions =+ 1 
+    }
 }
 
-function isSuspect () {
+function getStatus () {
     if (trueQuestions === 2) {return "Suspeita"}
     else if (trueQuestions === 3 || trueQuestions === 4 ) {return "Cúmplice"}
     else if (trueQuestions === 5) {return "Assassino"}
