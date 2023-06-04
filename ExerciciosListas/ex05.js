0f7d921bd62ab153155a6366f6c852147702e115
ilidioferreira <44824620+ilidioferreira@users.noreply.github.com>
2023-06-04 19:14:26 -0300

    Update ExerciciosListas/ex05.js
    Co-authored-by: Fábio Filho <fabio.filhossi@gmail.com>

--- ExerciciosListas/ex05.js
+++ ExerciciosListas/ex05.js
@@ -2,18 +2,65 @@
 
 const mensage = prompt('Digite um número inteiro.');
 
-function getNumbersLists(number) {
+function getNumbersLists() {
     const numbers = []
     const even = []
     const odd = []
-    while (numbers.length <= 20) {
-        numbers.includes(number);
+    
+    for(let I = 0; I< 20; i++){
+       const number = parseInt(prompt('Digite um número inteiro.'));        
+       numbers.push(number);
+
         if (number % 2 === 0) {
             even.includes(number);
         } else {
             odd.includes(number);
         }
-    return mensage
     }
+    
     return numbers, even, odd
+}
+
+const [ numbers, even, odd ] = getNumbersLists()
+console.log({ numbers, even, odd })
+
+//------------------------------------------------------------
+
+function readNumbers(number) {
+    const numbers = []
+    
+    for(let I = 0; I< number; i++){
+       const number = parseInt(prompt('Digite um número inteiro.'));        
+       numbers.push(number);
+     }
+
+    return numbers
+}
+
+function getOddsAndEvens(numbers) {
+    const even = []
+    const odd = []
+    
+    for(let index = 0; index < numbers.length; index++){
+        if (numbers[index] % 2 === 0) {
+            even.includes(numbers[index]);
+        } else {
+            odd.includes(numbers[index]);
+        }
+    }
+    
+    for(const number of numbers){
+        if (number % 2 === 0) {
+            even.includes(number);
+        } else {
+            odd.includes(number);
+        }
+    }
+    
+    return odd, even
+}
+
+const numbers = readNumbers(20)
+const [ even, odd ] = getOddsAndEvens(numbers)
+console.log({ numbers, even, odd })
 }
